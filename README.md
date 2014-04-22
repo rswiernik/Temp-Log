95ee03878318082ff358952cd24818256c8f1de9
reed <reed@ashkelon.csh.rit.edu>
2014-04-21 23:23:05 -0400

    Added comments, improving algorithm

--- README.md
+++ README.md
@@ -1,25 +1,26 @@
 Temp-Log
 ===
 
-** This project is a work in progress, network features and scrapping are still being worked on! **
+** This project is a work in progress, network features and scrapping are still being worked on!
 
-Temperature monitoring in perl using lm-sensors. This perl script scraps the output of the 'sensors' portion of lm-sensors. When run as the client, the script gathers core temperature data, means them, and sends the data to a master node. By default, when run as the client the program will send info to localhost. In the designed use-case, the server portion of Temps is run on the master node and each client whoes temperature is being monitored is then reported to the master. When run as the master node, the program should be run in the background awaiting outer node information.
+Temperature monitoring in perl using lm-sensors. This perl script scraps the output of the 'sensors' portion of lm-sensors. In the designed use-case, the server portion of Temps is run on the master node and each client whoes temperature is being monitored is then reported to the master. 
 
 Uasge as Client:
 ---
+When run as the client, the script gathers core temperature data, means them, and sends the data to a master node. By default, when run as the client the program will send info to localhost. 
 ```
 ~$ ./temps.pl -m <master node>
-			[-v | --verbose]
+			[-v | --version]
 			[-d | --debug]
                     
 ```
 
 Uasge as Server:
 ---
+When run as the master node, the program should be run in the background awaiting outer node information.
 ```
 ~$ ./temps.pl -s &
-			[-m <master node>]
-			[-v | --verbose]
+			[-v | --version]
 			[-d | --debug]
                     
 ```
--- temps.pl
+++ temps.pl
@@ -21,8 +21,11 @@ GetOptions ('debug' => \$DEBUG,
 			'master=i' => \$MASTER_NODE);
 
 if( $VER_CHK ){
+
 	print "TempsClient.pl version: $VERSION\n";
 	print "Author: Reed Swiernik\n";
+	exit 0;
+
 }
 
 my $hostname = `hostname`; chomp($hostname);
@@ -32,20 +35,24 @@ my $temp_line;
 # Iterate through the output of sensors looking
 # for the ISA adapter and core temps
 foreach( my $i=0; $i<@info; $i++ ){
+
 	# Grab and chomp the line
 	my $line = $info[$i]; chomp($line);
 	
 	# Check for coretemp in line
 	if( $line =~ m/coretemp/i ){
+		$i = $i + 1;
 		if($DEBUG){print "\t$line\n";}
 		# once weve found the ISA adapter, grab the next line,
 		# as that is where the adapter temperature is
 		# $temp_line = $info[$i+1]; chomp($temp_line);
-		while($line !~ m/^\n$/){
+		while($info[$i] !~ m/^\n$/){
+			if($DEBUG){print "Adding line: $line\n";}
 			$line = $info[$i]; chomp($line);
 			push(@data, $line);
 			$i = $i + 1;
 		}
+
 		# Quit out of checking
 		if($DEBUG){print @data;}
 		last;
@@ -56,13 +63,30 @@ if($DEBUG){print "Parsing Line: $temp_line\n";}
 
 my $temps = 0;
 if( $temp_line =~ m/\+([0-1]?[0-9]{2}\.0)\xc2\xb0[CF]/ ){
-	# Set 
+	# Regex for matching the sensors output
+	# -----
+	# m/\+([0-9]?{2}[0-9]\.0)\xc2\xb0[CF]/ ){
+	# 
+	# Here are some example ouputs that the regex matches:
+	# +100.0°C +35.0°F +23.0°C
+	#
+	# Written out:
+	# Plus sign followed by a number from 0 to 999 followed by a period, a 0,
+	# the degree sign (\xc2\xb0 is the hex value), and either a C or and F
+	# for the scale - returning the matched 235.0 value in the $1 variable
+	
+
+	# Set the matched portion that is the temp equal to the temps variable
 	$temps = $1;
+	
 	if($DEBUG){print "Temp: $temps\n";}
 	if($DEBUG){print "$hostname,$temps\n";}
+
 } else {
+
 	if($DEBUG){print "No temp found\n";}
 	exit 0;
+
 }
 
 
